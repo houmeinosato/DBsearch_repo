36aac87d013b31d860bd0596d45d3a1fb5846a25
shigeki matsunaga <111408444+houmeinosato@users.noreply.github.com>
2024-03-07 22:48:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def create_pdf():
     pdf.set_font("Arial", size=12)
     pdf.cell(200, 10, txt="Streamlit PDFダウンロードデモ", ln=True, align='C')
     # その他のPDFの内容をここに追加
-    return pdf.output(dest='S').encode( ‘utf-8’  )
+    return pdf.output(dest='S').encode( 'utf-8'  )
 
 # Base64でエンコードしてダウンロードリンクを生成する関数
 def create_download_link(pdf_content):
