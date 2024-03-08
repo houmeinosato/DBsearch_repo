075eb4ffda778df0d3b9d4435c8e3b9a8a99faf9
shigeki matsunaga <111408444+houmeinosato@users.noreply.github.com>
2024-03-09 06:54:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st
+from fpdf import FPDF
+import base64
+
+pdf = FPDF()  # pdf object
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.add_page()
+
+pdf.set_font("Times", "B", 18)
+pdf.set_xy(10.0, 20)
+pdf.cell(w=75.0, h=5.0, align="L", txt="This is my sample text")
+
+st.download_button(
+    "Download Report",
+    data=pdf.output(dest='S').encode('latin-1'),
+    file_name="Output.pdf",
+)
+
+
+'''
+import streamlit as st
 import base64
 from streamlit.components.v1 import html
 import numpy as np
@@ -26,6 +46,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
+
+
 
 '''
 import streamlit as st
