63524d3b4e461eb83a53050373752326efddda13
shigeki matsunaga <111408444+houmeinosato@users.noreply.github.com>
2024-03-09 16:14:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ pdf.add_page()
 pdf.set_font("Times", "B", 11)
 pdf.set_xy(10.0, 20)
 pdf.cell(w=75.0, h=5.0, align="L", txt="This is my sample text")
-pdf.set_font("Times", , 13)
+pdf.set_font("Times",  13)
 pdf.set_xy(20.0, 30)
 pdf.cell(w=75.0, h=5.0, align="L", txt="This is my sample text")
 
