e6ea1b5b715bedf07531deb77e43e8e56aaaaefb
shigeki matsunaga <111408444+houmeinosato@users.noreply.github.com>
2024-03-09 20:23:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-from reportlab.lib import colors
+import streamlit as st
+import pdfkit
+import reportlab
 
-doc = SimpleDocTemplate("table_pdf.pdf", pagesize=letter)
-data = [["col1", "col2", "col3"],
-        [1, 2, 3],
-        [4, 5, 6]]
+# Streamlitのタイトルと説明文
+st.title("StreamlitでPDF作成")
+st.markdown("このアプリでは、StreamlitでPDFファイルを作成する方法を紹介します。")
 
-table = Table(data)
-table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-                           ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-                           ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                           ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                           ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                           ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-                           ('GRID', (0, 0), (-1, -1), 1, colors.black)]))
+# StreamlitのファイルアップローダーでHTMLファイルを選択
+uploaded_file = st.file_uploader("HTMLファイルを選択してください", type="html")
 
-elements = [table]
-doc.build(elements)
+# HTMLファイルが選択された場合
+if uploaded_file is not None:
+    # HTMLファイルの内容を読み込む
+    html = uploaded_file.read()
+
+    # PDFファイルの名前を入力する
+    pdf_name = st.text_input("PDFファイルの名前を入力してください", value="output.pdf")
+
+    # PDFファイルの作成ボタンを表示する
+    create_button = st.button("PDFファイルを作成")
+
+    # PDFファイルの作成ボタンが押された場合
+    if create_button:
+        # pdfkitを使ってHTMLファイルをPDFファイルに変換する
+        pdfkit.from_string(html, pdf_name)
+
+        # reportlabを使ってPDFファイルをバイナリデータに変換する
+        with open(pdf_name, "rb") as f:
+            pdf_data = f.read()
+
+        # StreamlitのダウンロードボタンでPDFファイルをダウンロードできるようにする
+        st.download_button("PDFファイルをダウンロード", pdf_data, file_name=pdf_name, mime="application/pdf")
