95344376d30724052310de6b10f538a5c47fc0d7
shigeki matsunaga <111408444+houmeinosato@users.noreply.github.com>
2024-03-09 18:08:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,10 @@ pdf.set_font("Times", "B", 11)
 pdf.set_xy(10.0, 20)
 pdf.cell(w=75.0, h=5.0, align="L", txt="This is my sample text")
 
-pdf.set_xy(20.0, 30)
-pdf.cell(w=75.0, h=5.0, align="L", txt="これは　私のサンプル文章です")
 
 st.download_button(
     "Download Report",
-    #data=pdf.output(dest='S').encode('latin-1'),
-    data=pdf.output(dest='S').encode('utf-8'),
+    data=pdf.output(dest='S').encode('latin-1'),
 
     file_name="Output.pdf",
 )
