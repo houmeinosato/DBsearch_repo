699824deb78eb85e239672a08e72543a82decae0
shigeki matsunaga <111408444+houmeinosato@users.noreply.github.com>
2024-03-09 18:34:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ import streamlit as st
 from fpdf import FPDF
 import base64
 
-pdf = FPDF()  # pdf object
+pdf = FPDF()  # PDFオブジェクトを作成
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
-
 pdf.set_font("Times", "B", 11)
 pdf.set_xy(10.0, 20)
-pdf.cell(w=75.0, h=5.0, align="L", txt="This is my sample text")
+pdf.cell(w=75.0, h=5.0, align="L", txt="これはサンプルテキストです")
 
-
+# ダウンロードボタンを表示
 st.download_button(
-    "Download Report",
+    "レポートをダウンロード",
     data=pdf.output(dest='S').encode('latin-1'),
-
     file_name="Output.pdf",
 )
-
 
